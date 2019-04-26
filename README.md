392d337cd6dbc8f71f01f825ab82c9bb40170bfb
leonardo <l.frioli@studenti.unipi.it>
2019-04-26 15:55:06 +0200

    Added the report.

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 # Kademmlia assignment
 
 This is the implementation of the midterm assignment given in the course of Peer to Peer Systems and Blockchains at the university of Pisa.
+
+To build the code run  
+
+``` ant -f build.xml  ```
+
+Then you can execute the python script.
--- run_statistics.py
+++ run_statistics.py
@@ -2,12 +2,17 @@
 import os
 from subprocess import Popen
 
+#
+# This script can be used to automate the tests
+# Before running it you must build the project
+# $> ant -f build.xml
+#
+
 k_values = [2,8,20]
 n_values = [250, 500, 1000, 2000, 3000]
 m_values = [16, 32, 64]
 
 os.system("rm -rf ./statistics")
-#os.system('java -classpath "./build/classes:./gephi-toolkit-0.9.2-all.jar:./jsoup-1.11.3.jar" midterm.Main  16 100 10')
 
 javacmd = 'java -classpath "./build/classes:./gephi-toolkit-0.9.2-all.jar:./jsoup-1.11.3.jar" midterm.Main {} {} {}'
 
